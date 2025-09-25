89331bdf9076ae3b91c5e2c254064016eefa303b
jm <joy020926@gmail.com>
2025-09-26 05:01:48 +0900

    final

--- bin/assignment_1.dart
+++ bin/assignment_1.dart
@@ -1,8 +1,5 @@
-import 'package:assignment_1/assignment_1.dart' as assignment_1;
-
 void main() {
   int score = 84;
-
   String grade;
   if (score >= 90 && score <= 100) {
     grade = 'A등급';
@@ -11,6 +8,31 @@ void main() {
   } else {
     grade = 'C등급';
   }
-
   print('이 학생의 점수는 $score이며, 등급은 $grade입니다!');
+
+  Map<String, int> products = {
+    "티셔츠": 10000,
+    "바지": 8000,
+    "모자": 4000,
+  };
+  List<String> cart = ["티셔츠", "바지", "모자", "티셔츠", "바지"];
+
+  calculateCart(products, cart);
+}
+
+void calculateCart(Map<String, int> products, List<String> cart) {
+  int totalPrice = 0;
+  for (var item in cart) {
+    totalPrice += products[item] ?? 0;
+  }
+
+  double discount = 0; 
+  print('장바구니에 ${totalPrice}원 어치를 담으셨네요 !');
+  if (totalPrice > 20000) {
+    discount = totalPrice * 0.1; 
+    print('할인 금액 : ${discount.toInt()}원');
+  }
+
+  double finalPrice = totalPrice - discount;
+  print('최종 결제 금액은 ${finalPrice.toInt()}원입니다!');
 }
