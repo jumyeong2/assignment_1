c33e5e08854d5af3bfd593a1947834046c485064
jm <joy020926@gmail.com>
2025-09-26 04:38:46 +0900

    score

--- bin/assignment_1.dart
+++ bin/assignment_1.dart
@@ -1,5 +1,16 @@
 import 'package:assignment_1/assignment_1.dart' as assignment_1;
 
-void main(List<String> arguments) {
-  print('Hello: ${assignment_1.calculate()}!');
+void main() {
+  int score = 84;
+
+  String grade;
+  if (score >= 90 && score <= 100) {
+    grade = 'A등급';
+  } else if (score >= 80 && score <= 89) {
+    grade = 'B등급';
+  } else {
+    grade = 'C등급';
+  }
+
+  print('이 학생의 점수는 $score이며, 등급은 $grade입니다!');
 }
