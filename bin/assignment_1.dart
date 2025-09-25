7c5f815446dd822a8a7c27b2f44f261bf81e9813
jm <joy020926@gmail.com>
2025-09-25 12:55:06 +0900

    hello 수정

--- bin/assignment_1.dart
+++ bin/assignment_1.dart
@@ -1,5 +1,5 @@
 import 'package:assignment_1/assignment_1.dart' as assignment_1;
 
 void main(List<String> arguments) {
-  print('Hello world: ${assignment_1.calculate()}!');
+  print('Hello: ${assignment_1.calculate()}!');
 }
